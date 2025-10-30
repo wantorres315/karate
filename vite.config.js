866ec84989845616cdc54c382eebd150879ac0e7
Wanessa <wanessa.araujo.torres@gmail.com>
2025-10-30 11:16:35 +0000

    index

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/camera.js', 'resources/js/profile-photo.js', ],
-            refresh: true,
-        }),
+  plugins: [laravel({
+    input: [
+      'resources/css/app.css',
+      'resources/js/app.js',
+      'resources/js/camera.js', // adicione esta linha se faltar
+      'resources/js/profile-photo.js'
     ],
+    refresh: true,
+  })]
 });
